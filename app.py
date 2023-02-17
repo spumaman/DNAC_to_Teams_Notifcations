48ea237a4f64cb87b05f67ef6f2a3fca23e996e2
Adam Stokes <stokes_adam@hotmail.com>
2023-02-17 16:22:00 +1300

    Update app.py

--- app.py
+++ app.py
@@ -14,12 +14,15 @@ def webhook():
  if request.method == 'POST':
   json_webhook = request.json
 
-# Parse on the details section of the payload of the alert put each key\value into pre_payload, add new lines so it looks pretty on the MS Teams channel
+# Parse the details section of the alert payload sent by DNAC, put each key\value into pre_payload, add new lines so it looks pretty on the MS Teams channel
   pre_payload = "\\\n ".join(key +': ' + value.capitalize() for key,value in json_webhook['details'].items())
+ 
 # Create a payload with the JSON object "Text": and the value of the pre_payload as a string.
   payload = {"text": str(pre_payload)}
+ 
 # Send the payload off to MS Teams webhook URL for it to display in your chosen channel
   response = requests.post(teams_webhook, json=payload)
+ 
 # Print the payload for debugging purposes can comment out.
   print(payload)
   return "Webhook received!"
