a584c1cbb6e96f0bffad819e9dde823ab27a6628
Adam Stokes <stokes_adam@hotmail.com>
2023-02-20 16:41:19 +1300

    Update app.py
    Added a bit of formatting (bold and h1 tags) with the addition of make the Assurance Issue Name being the Title of the webhook/MS Team Card

--- app.py
+++ app.py
@@ -14,18 +14,21 @@ def webhook():
  if request.method == 'POST':
   json_webhook = request.json
 
-# Parse the details section of the alert payload sent by DNAC, put each key\value of that section into pre_payload, add new lines so it looks pretty on the MS Teams channel
-  pre_payload = "\\\n ".join(key +': ' + value.capitalize() for key,value in json_webhook['details'].items())
- 
-# Create a payload with the JSON object "Text": and the value of the pre_payload as a string.
-  payload = {"text": str(pre_payload)}
- 
+  #Grab Assurance name value to make it the Title of the notification
+  alert_title = '<h1><b>' + json_webhook['details']['Assurance Issue Name'] + '</b></h1> '
+
+# Parse on the details section of the payload of the alert put each key\value into pre_payload, add new lines so it looks pretty on the MS Teams channel
+  pre_payload = "\\\n".join('<b>'+ key + ':</b> ' + value for key,value in json_webhook['details'].items())
+
+# Create a payload with the JSON object "Title" and "Text": with the values of the alert_title and pre_payload as strings.
+  payload = {"title": str(alert_title),"text":str(pre_payload) }
+
 # Send the payload off to MS Teams webhook URL for it to display in your chosen channel
   response = requests.post(teams_webhook, json=payload)
- 
+
 # Print the payload for debugging purposes can comment out.
-  print(payload)
+# print(payload)
+
   return "Webhook received!"
  
-#Spin up the Flask instance using ssl adhoc (only use adhoc for testing, never use in a production environment)
 app.run(host='0.0.0.0', port=8000, ssl_context='adhoc')
